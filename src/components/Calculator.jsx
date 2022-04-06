4a852e67be4c28dec1f2601c399fc280bc102352
FNuno <65033083+FNunoGM@users.noreply.github.com>
2022-04-06 15:54:20 +0100

    Update Calculator.jsx

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -8,27 +8,27 @@ export default function Calculator() {
 
   function inputNum(e) {        //This function inputs the values with some conditions
     var input = e.target.value; 
-    if (num===0){               //it will input a value if screen value is 0
-      setNum(input);
+    if (num===0){               //it will input a value if screen value is 0 and replace it
+      setNum(input);            //and it will save the value in memory
     }else{                      //else it will concatenate with the previous input
       setNum(num + input);
     }
   } 
 
-  function reset() {  //Function that resets current value and the previous one
+  function reset() {            //Function that resets current value and the previous one
     setNum(0)
     setPreviousNum(0)
   }
 
-  function operatorType(e) {  //Function that inputs the operators
+  function operatorType(e) {    //Function that inputs the operators
     var operatorInput = e.target.value;
-    setOperator(operatorInput)
-    setPreviousNum(num);      //The current value will be stored when an operator is inputed
-    setNum(0);                //The value in the screen will return to zero 
+    setOperator(operatorInput)  //This will save the chosen operator in memory
+    setPreviousNum(num);        //The current value will be stored when an operator is inputed
+    setNum(0);                  //The value in the screen will return to zero 
   }
 
-  function calculate() {  // This the function that calculates based on the operator
-    if(operator==="/") {  // At first i tried without parseFloat, but kept getting some errors 
+  function calculate() {        // This the function that calculates based on the operator
+    if(operator==="/") {        // At first i tried without parseFloat, but kept getting some errors 
       setNum(parseFloat(previousNum) / parseFloat(num));
     } else if (operator === "x") {
       setNum(parseFloat(previousNum) * parseFloat(num));
